9cd1b55f0f70602964aa6aeb35b413054ec52dd4
Maz-dev <contact@maz-dev.cc>
2015-01-09 17:48:33 +0100

    Added local db settings

--- messageo/settings.py
+++ messageo/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'messageo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'messageo',
+        'USER': 'maz-dev',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
